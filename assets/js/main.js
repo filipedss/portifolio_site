783cce7fb9f6de07c211ddc9f31625b6dd698533
Filipe Santos <filipssant@gmail.com>
2023-05-02 12:50:20 -0300

    close menu clicking on an icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 /* show and hide menu */
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+      navClose = document.getElementById('nav-close'),
+      navLink = document.querySelectorAll('.nav__link')
 
 /* if element exist */
 if(navToggle){
@@ -20,3 +21,11 @@ if(navClose){
     navMenu.classList.remove('show-menu')
   })
 }
+
+/* remove show css class */
+function linkAction(){
+  navMenu.classList.remove('show-menu')
+}
+
+/* add event listener to all menu links*/
+navLink.forEach(n => n.addEventListener('click', linkAction))
