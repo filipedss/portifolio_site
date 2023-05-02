75c33eb1724ef5d3ec77ebb80f2576209d23613a
Filipe Santos <filipssant@gmail.com>
2023-05-02 11:34:28 -0300

    hide menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,20 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/* validar se a constante existe */
+/* if element exist */
 if(navToggle){
+  /*add event click on toggle element*/
   navToggle.addEventListener('click', function() {
+    /*add show css class*/
     navMenu.classList.add('show-menu')
   });
 }
 
+/* if close element exist */
+if(navClose){
+  /*add event click on toggle element*/
+  navClose.addEventListener('click', function () {
+    /*remove show css class*/
+    navMenu.classList.remove('show-menu')
+  })
+}
